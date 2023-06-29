0c65cd3d0b7331d4a238c63abea557749ec2061f
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-06-29 10:09:34 +0200

    RouterLink y RouterLinkActiveOptions

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
@@ -8,14 +11,19 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent,
   ],
 })
 export class SharedModule { }
