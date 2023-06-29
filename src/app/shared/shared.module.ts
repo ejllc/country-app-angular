b3594bd721a0aab79fac4df548b73268e1272356
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-06-29 09:24:27 +0200

    routermodule y paginas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent
+  ],
 })
 export class SharedModule { }
