5cd3993bafa1007f1c2fd649bce27aaeef423c83
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-06-30 11:44:04 +0200

    LoadingSpinner

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of, map } from 'rxjs';
+import { Observable, catchError, of, map, delay } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -12,10 +12,16 @@ export class CountriesService {
 
   constructor(private httpClient: HttpClient) { }
 
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(url)
+      .pipe(
+        catchError(() => of([])),
+        delay(2000),
+      );
+  }
+
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
-
     const url = `${this.apiUrl}/alpha/${code}`;
-
     return this.httpClient.get<Country[]>(url)
       .pipe(
         map(countries => countries.length > 0 ? countries[0] : null),
@@ -24,34 +30,18 @@ export class CountriesService {
   }
 
   searchCapital(term: string): Observable<Country[]> {
-
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.httpClient.get<Country[]>(url)
-      .pipe(
-        catchError(error => {
-          console.log(error)
-          return of([])
-        }));
+    return this.getCountriesRequest(url);
   }
 
   searchCountry(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`;
-    return this.httpClient.get<Country[]>(url)
-      .pipe(
-        catchError(error => {
-          console.log(error)
-          return of([])
-        }));
+    return this.getCountriesRequest(url);
   }
 
   searchRegion(region: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${region}`;
-    return this.httpClient.get<Country[]>(url)
-      .pipe(
-        catchError(error => {
-          console.log(error)
-          return of([])
-        }));
+    return this.getCountriesRequest(url);
   }
 
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 
 
@@ -15,7 +16,8 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     AboutPageComponent,
     SidebarComponent,
     ContactPageComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    LoadingSpinnerComponent,
   ],
   imports: [
     CommonModule,
@@ -27,6 +29,7 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     ContactPageComponent,
     SidebarComponent,
     SearchBoxComponent,
+    LoadingSpinnerComponent,
   ],
 })
 export class SharedModule { }
